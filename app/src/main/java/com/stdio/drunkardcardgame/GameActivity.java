eb21e22cc7818a24fe1c4c1309cff7337d69123b
Std1o <37378410+Std1o@users.noreply.github.com>
2021-02-12 19:02:14 +0300

    Update GameActivity.java

--- app/src/main/java/com/stdio/drunkardcardgame/GameActivity.java
+++ app/src/main/java/com/stdio/drunkardcardgame/GameActivity.java
@@ -5,12 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class GameActivity extends AppCompatActivity {
+
+    private List<CardModel> playerCards = new ArrayList<>();
+    private List<CardModel> aiCards = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        Toast.makeText(this, "" + new CardsListHelper().getCards().size(), Toast.LENGTH_SHORT).show();
+        distributeCards(new CardsListHelper().getCards(), 18);
+        for (CardModel cardModel : playerCards) {
+            System.out.println(cardModel.getSuit() + " " + cardModel.getWeight());
+        }
+    }
+
+    private void distributeCards(ArrayList<CardModel> lst, int n) {
+        List<CardModel> copy = new ArrayList<>(lst);
+        Collections.shuffle(copy);
+        playerCards = copy.subList(0, n);
+        aiCards = copy.subList(n, copy.size());
     }
 }
